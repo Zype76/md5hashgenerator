560f0ba755f078222eabed7e540b8d679385e3da
Zype76 <zype76@gmail.com>
2016-04-21 19:51:31 -0400

    I added a basic permutation generator

---
+++ generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from itertools import product
+from string import ascii_lowercase
+keywords = [''.join(i) for i in product(ascii_lowercase, repeat = 3)]
+
+print keywords
+
+
+from itertools import product
+from string import ascii_lowercase
+keywords = [''.join(i) for i in product(ascii_lowercase, repeat = 3)]
+
+print keywords
